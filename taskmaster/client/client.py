8cd8cf9fad299a1d29fa4033b566915f23725372
yohan-th <ythollet@student.le-101.fr>
2019-12-01 17:22:30 +0200

    imp: client class

--- taskmaster/client/client.py
+++ taskmaster/client/client.py
@@ -1,30 +1,55 @@
 import cmd
 import os
+from taskmaster.common import config as tm_config
+from taskmaster.utils import log
+from getpass import getpass
+import socket
 
-class TaskmasterShell(cmd.Cmd):
-    intro = 'Welcome to the taskmaster shell.   Type help or ? to list commands.\n'
-    prompt = '(taskmaster)$> '
+log = log.get_logger('server')
 
-    def default(self, line):
-        print("return value : " + line)
+class Client():
 
-    def do_shell(self, line):
-        "Run a shell command"
-        print("running shell command:", line)
-        output = os.popen(line).read()
-        print(output)
+    def __init__(self, configFile : str = None):
+        self.config = tm_config.ConfigClient(configFile)
 
-    def do_args(self, args):
-        print(args)
+    def start(self):
+        log.info('running the client daemon')
+        attempt = 0
+        while True:
+            if self.connect() == False:
+                attempt += 1
+                log.error('failed connection to server. Attempt:'+attempt)
+                if attempt == 3:
+                    return False
+            else:
+                break
+        log.info('created connection to server')
+        attempt = 0
+        while True:
+            if not self.authenticate():
+                attempt += 1
+                log.error('failed authentication to server. Attempt:'+attempt)
+                if attempt == 3:
+                    return False
+            else:
+                break
+        log.info('authenticated to server')
 
-    def do_bye(self, line):
-        "Exit"
+    def connect(self):
+        if not self.config.host:
+            self.config.host = input("Enter host ip: ")
+        if not self.config.port:
+            self.config.port = input("Enter port: ")
+        # if not socket.create_connection((self.config.host, self.config.host)):
+        #     print('Failed connection to server.')
         return True
 
-    def do_EOF(self, line):
-        "Exit with Ctrl-D"
+    def authenticate(self):
+        if not self.config.username:
+            self.config.username = input("Enter username: ")
+        if not self.config.password:
+            self.config.password = getpass("Enter password: ")
+        # if not socket.AUTHENTIFICATE(username + password):
+        #     print('Failed to authentificate user'+self.config.username)
+        #     return False
         return True
-
-
-if __name__ == '__main__':
-    TaskmasterShell().cmdloop()
